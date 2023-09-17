5bb4e8cb7e00c5bbbac7ac2bd1638faa32753e05
USERSAD\sojustin <sojustin@emeal.nttdata.com>
2023-09-16 22:07:41 -0300

    Chore: Creo estructura base de proyecto.

---
+++ Backend/src/test/kotlin/divi/cuentas/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package divi.cuentas.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
